d258acd4111d14631dfe87c65a15c8f86810515b
lautiromero <lautiromero94@gmail.com>
2022-02-28 11:49:29 -0300

    first commit

---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Cursos create')
+
+@section('content')
+    <h1>Esta es la pagina para crear un curso</h1>
+@endsection()
---
+++ resources/views/cursos/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title', 'Cursos')
+
+@section('content')
+    <h1>Welcome to the course home page</h1>
+@endsection()
